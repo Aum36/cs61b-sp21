a41c9620c03b60fac922890a0b1b39ecb74816c8
Aum Kulkarni <aumwinay@gmail.com>
2025-06-14 02:31:29 +0530

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,16 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+    /** Return the next number in the Collatz sequence
+     * @param number
+     * if number is odd, next number is 3 * number + 1
+     * if number is even, next number is n / 2
+     */
+    public static int nextNumber(int number) {
+        if (number % 2 == 0) {
+            return number / 2;
         } else {
-            return n * 2;
+            return 3 * number + 1;
         }
     }
 
