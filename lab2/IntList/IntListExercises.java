a97466683c4b329814b930d29fb679b3bcd6f100
Aum Kulkarni <aumwinay@gmail.com>
2025-07-04 17:47:29 +0530

    Checking if everything works now

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -17,7 +17,7 @@ public class IntListExercises {
     }
 
     /**
-     * Part B: Buggy method that sets node.first to zero if
+     * Part B: Buggy method that sets node first to zero if
      * the max value in the list starting at node has the same
      * first and last digit, for every node in L
      *
